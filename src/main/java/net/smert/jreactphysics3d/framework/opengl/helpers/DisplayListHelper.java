5dde1c48e96de5d140bea3531575be369017dd6b
Jason Sorensen <git@smert.net>
2014-09-17 09:02:32 -0400

    Removed final from methods.

--- src/main/java/net/smert/jreactphysics3d/framework/opengl/helpers/DisplayListHelper.java
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/helpers/DisplayListHelper.java
@@ -8,23 +8,23 @@ import org.lwjgl.opengl.GL11;
  */
 public class DisplayListHelper {
 
-    public final void begin(int id) {
+    public void begin(int id) {
         GL11.glNewList(id, GL11.GL_COMPILE);
     }
 
-    public final void call(int id) {
+    public void call(int id) {
         GL11.glCallList(id);
     }
 
-    public final int create() {
+    public int create() {
         return GL11.glGenLists(1);
     }
 
-    public final void delete(int id) {
+    public void delete(int id) {
         GL11.glDeleteLists(id, 1);
     }
 
-    public final void end() {
+    public void end() {
         GL11.glEndList();
     }
 
