5ee7b7149d99e8570537432dbfa3c6723b6290b3
Jason Sorensen <git@smert.net>
2014-09-18 08:19:38 -0400

    Add missing interface for InputProcessor.

---
+++ src/main/java/net/smert/jreactphysics3d/framework/helpers/InputProcessor.java
@@ -0,0 +1,13 @@
+package net.smert.jreactphysics3d.framework.helpers;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public interface InputProcessor {
+
+    public void registerActions();
+
+    public void unregisterActions();
+
+}
