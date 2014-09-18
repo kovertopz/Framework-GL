083dc33cc87d4967c1c302a65cd515ea23e29294
Jason Sorensen <git@smert.net>
2014-09-18 05:52:02 -0400

    Added a constant.

--- src/main/java/net/smert/jreactphysics3d/framework/math/MathHelper.java
+++ src/main/java/net/smert/jreactphysics3d/framework/math/MathHelper.java
@@ -9,6 +9,7 @@ public class MathHelper {
     public final static float DEG_TO_RAD = (float) Math.PI / 180.0f;
     public final static float FLOAT_EPSILON = 1.1920928955078125E-7f;
     public final static float PI_OVER_2 = (float) Math.PI / 2.0f;
+    public final static float PI_OVER_180 = (float) Math.PI / 180.0f;
     public final static float PI_OVER_360 = (float) Math.PI / 360.0f;
     public final static float RAD_TO_DEG = 180.0f / (float) Math.PI;
     public final static float TOLERANCE_DOT_PRODUCT_PARALLEL = 0.999999f;
