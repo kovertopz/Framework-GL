3049f61a2ac3c4ef6e6150a56f03bcf9302996e1
Jason Sorensen <git@smert.net>
2014-09-22 18:44:35 -0400

    Added several add methods to add data to the segment.

--- src/main/java/net/smert/jreactphysics3d/framework/opengl/mesh/Segment.java
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/mesh/Segment.java
@@ -13,20 +13,64 @@ public class Segment {
 
     private int maxIndex;
     private int minIndex;
-    private final int primitive;
+    private final int primitiveMode;
     private final List<Vector4f> colors;
     private final List<Vector3f> normals;
     private final List<Vector3f> texCoords;
     private final List<Vector4f> vertices;
 
-    public Segment(int primitive) {
+    public Segment(int primitiveMode) {
         maxIndex = 0;
         minIndex = 0;
-        this.primitive = primitive;
+        this.primitiveMode = primitiveMode;
         colors = new ArrayList<>();
         normals = new ArrayList<>();
         texCoords = new ArrayList<>();
         vertices = new ArrayList<>();
+    }
+
+    public void addColor(float x, float y, float z) {
+        colors.add(new Vector4f(x, y, z, 1.0f));
+    }
+
+    public void addColor(float x, float y, float z, float w) {
+        colors.add(new Vector4f(x, y, z, w));
+    }
+
+    public void addColor(Vector4f vector) {
+        colors.add(vector);
+    }
+
+    public void addNormal(float x, float y, float z) {
+        normals.add(new Vector3f(x, y, z));
+    }
+
+    public void addNormal(Vector3f vector) {
+        normals.add(vector);
+    }
+
+    public void addTexCoord(float x, float y) {
+        texCoords.add(new Vector3f(x, y, 0));
+    }
+
+    public void addTexCoord(float x, float y, float z) {
+        texCoords.add(new Vector3f(x, y, z));
+    }
+
+    public void addTexCoord(Vector3f vector) {
+        texCoords.add(vector);
+    }
+
+    public void addVertex(float x, float y, float z) {
+        vertices.add(new Vector4f(x, y, z, 1.0f));
+    }
+
+    public void addVertex(float x, float y, float z, float w) {
+        vertices.add(new Vector4f(x, y, z, w));
+    }
+
+    public void addVertex(Vector4f vector) {
+        vertices.add(vector);
     }
 
     public int getMaxIndex() {
@@ -45,8 +89,8 @@ public class Segment {
         this.minIndex = minIndex;
     }
 
-    public int getPrimitive() {
-        return primitive;
+    public int getPrimitiveMode() {
+        return primitiveMode;
     }
 
     public List<Vector4f> getColors() {
