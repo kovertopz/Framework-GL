4ef955f4ce642cd6570c67023b515c292261c0a9
Jason Sorensen <git@smert.net>
2014-09-22 13:02:20 -0400

    Changed classes to rely on GL directly.

--- src/main/java/net/smert/jreactphysics3d/framework/opengl/DisplayList.java
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/DisplayList.java
@@ -1,6 +1,5 @@
 package net.smert.jreactphysics3d.framework.opengl;
 
-import net.smert.jreactphysics3d.framework.opengl.helpers.DisplayListHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +12,6 @@ public class DisplayList {
     private final static Logger log = LoggerFactory.getLogger(DisplayList.class);
 
     private int displayListID;
-    private static DisplayListHelper displayListHelper;
 
     public DisplayList() {
         displayListID = 0;
@@ -21,23 +19,19 @@ public class DisplayList {
 
     public void create() {
         destroy();
-        displayListID = displayListHelper.create();
+        displayListID = GL.displayListHelper.create();
         log.debug("Created a new display list with ID: {}", displayListID);
     }
 
     public void destroy() {
         if (displayListID != 0) {
-            displayListHelper.delete(displayListID);
+            GL.displayListHelper.delete(displayListID);
             log.debug("Deleted a display list with ID: {}", displayListID);
         }
     }
 
     public int getDisplayListID() {
         return displayListID;
-    }
-
-    public static void SetDisplayListHelper(DisplayListHelper displayListHelper) {
-        DisplayList.displayListHelper = displayListHelper;
     }
 
 }
--- src/main/java/net/smert/jreactphysics3d/framework/opengl/VertexBufferObject.java
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/VertexBufferObject.java
@@ -1,6 +1,5 @@
 package net.smert.jreactphysics3d.framework.opengl;
 
-import net.smert.jreactphysics3d.framework.opengl.helpers.VertexBufferObjectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +12,6 @@ public class VertexBufferObject {
     private final static Logger log = LoggerFactory.getLogger(VertexBufferObject.class);
 
     private int vboID;
-    private static VertexBufferObjectHelper vboHelper;
 
     public VertexBufferObject() {
         vboID = 0;
@@ -21,23 +19,19 @@ public class VertexBufferObject {
 
     public void create() {
         destroy();
-        vboID = vboHelper.create();
+        vboID = GL.vboHelper.create();
         log.debug("Created a new VBO with ID: {}", vboID);
     }
 
     public void destroy() {
         if (vboID != 0) {
-            vboHelper.delete(vboID);
+            GL.vboHelper.delete(vboID);
             log.debug("Deleted a VBO with ID: {}", vboID);
         }
     }
 
     public int getVboID() {
         return vboID;
-    }
-
-    public static void SetVertexBufferObjectHelper(VertexBufferObjectHelper vboHelper) {
-        VertexBufferObject.vboHelper = vboHelper;
     }
 
 }
