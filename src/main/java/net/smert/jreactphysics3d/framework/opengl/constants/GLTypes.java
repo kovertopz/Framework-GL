162cfb4baffe85ed2321c19eff35b15838e7e290
Jason Sorensen <git@smert.net>
2014-09-22 09:19:55 -0400

    Added new constants.

---
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/constants/GLTypes.java
@@ -0,0 +1,23 @@
+package net.smert.jreactphysics3d.framework.opengl.constants;
+
+import org.lwjgl.opengl.GL11;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class GLTypes {
+
+    public final static int BYTE = GL11.GL_BYTE;
+    public final static int UNSIGNED_BYTE = GL11.GL_UNSIGNED_BYTE;
+    public final static int DOUBLE = GL11.GL_DOUBLE;
+    public final static int FLOAT = GL11.GL_FLOAT;
+    public final static int INT = GL11.GL_INT;
+    public final static int UNSIGNED_INT = GL11.GL_UNSIGNED_INT;
+    public final static int SHORT = GL11.GL_SHORT;
+    public final static int UNSIGNED_SHORT = GL11.GL_UNSIGNED_SHORT;
+
+    private GLTypes() {
+    }
+
+}
