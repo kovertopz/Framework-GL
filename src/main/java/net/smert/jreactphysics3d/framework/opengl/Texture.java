8fbb1dba402e913bbff0a8915cfd20abc96da3dc
Jason Sorensen <git@smert.net>
2014-09-27 18:15:47 -0400

    Texture now creates and destroys a texture ID.

--- src/main/java/net/smert/jreactphysics3d/framework/opengl/Texture.java
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/Texture.java
@@ -12,10 +12,38 @@
  */
 package net.smert.jreactphysics3d.framework.opengl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author Jason Sorensen <sorensenj@smert.net>
  */
 public class Texture {
+
+    private final static Logger log = LoggerFactory.getLogger(Texture.class);
+
+    private int textureID;
+
+    public Texture() {
+        textureID = 0;
+    }
+
+    public void create() {
+        destroy();
+        textureID = GL.textureHelper.create();
+        log.debug("Created a new texture with ID: {}", textureID);
+    }
+
+    public void destroy() {
+        if (textureID != 0) {
+            GL.textureHelper.delete(textureID);
+            log.debug("Deleted a texture with ID: {}", textureID);
+        }
+    }
+
+    public int getTextureID() {
+        return textureID;
+    }
 
 }
