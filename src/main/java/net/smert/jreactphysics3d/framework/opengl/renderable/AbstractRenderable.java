1fe85a457e829a61c4f6da0684fff690d5423c80
Jason Sorensen <git@smert.net>
2014-09-28 02:20:20 -0400

    AbstractRenderable no longer holds a reference to mesh.

--- src/main/java/net/smert/jreactphysics3d/framework/opengl/renderable/AbstractRenderable.java
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/renderable/AbstractRenderable.java
@@ -12,19 +12,10 @@
  */
 package net.smert.jreactphysics3d.framework.opengl.renderable;
 
-import net.smert.jreactphysics3d.framework.opengl.renderable.gl1.Renderable;
-import net.smert.jreactphysics3d.framework.opengl.mesh.Mesh;
-
 /**
  *
  * @author Jason Sorensen <sorensenj@smert.net>
  */
 public abstract class AbstractRenderable implements Renderable {
-
-    protected Mesh mesh;
-
-    public AbstractRenderable(Mesh mesh) {
-        this.mesh = mesh;
-    }
 
 }
