183892fb15a5535e1ce7421ede7c227b776fbd37
Jason Sorensen <git@smert.net>
2014-09-28 02:43:22 -0400

    New static instance class. Holds previously hidden bind states and other classes used during building of renderables.

---
+++ src/main/java/net/smert/jreactphysics3d/framework/opengl/renderable/factory/RenderableFactory.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2012 Jason Sorensen (sorensenj@smert.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package net.smert.jreactphysics3d.framework.opengl.renderable.factory;
+
+import net.smert.jreactphysics3d.framework.opengl.renderable.gl1.DrawCommands;
+import net.smert.jreactphysics3d.framework.opengl.renderable.va.VABindState;
+import net.smert.jreactphysics3d.framework.opengl.renderable.va.VABuilder;
+import net.smert.jreactphysics3d.framework.opengl.renderable.va.VertexArrays;
+import net.smert.jreactphysics3d.framework.opengl.renderable.vbo.ByteBuffers;
+import net.smert.jreactphysics3d.framework.opengl.renderable.vbo.VBOBindState;
+import net.smert.jreactphysics3d.framework.opengl.renderable.vbo.VBOBuilder;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class RenderableFactory {
+
+    public static ByteBuffers byteBuffers;
+    public static DrawCommands drawCommandsConversion;
+    public static RenderableConfiguration config;
+    public static VABindState vaBindState;
+    public static VABuilder vaBuilder;
+    public static VBOBindState vboBindState;
+    public static VBOBuilder vboBuilder;
+    public static VertexArrays vertexArrays;
+
+}
