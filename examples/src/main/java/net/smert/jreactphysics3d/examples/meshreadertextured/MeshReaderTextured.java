0c67d04ab693fe8baf90a13a47e73bb2ac8a71fa
Jason Sorensen <git@smert.net>
2014-10-07 04:55:39 -0400

    Added various examples for the framework.

---
+++ examples/src/main/java/net/smert/jreactphysics3d/examples/meshreadertextured/MeshReaderTextured.java
@@ -0,0 +1,172 @@
+/**
+ * Copyright 2014 Jason Sorensen (sorensenj@smert.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package net.smert.jreactphysics3d.examples.meshreadertextured;
+
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import net.smert.jreactphysics3d.framework.Fw;
+import net.smert.jreactphysics3d.framework.Screen;
+import net.smert.jreactphysics3d.framework.helpers.Keyboard;
+import net.smert.jreactphysics3d.framework.opengl.GL;
+import net.smert.jreactphysics3d.framework.opengl.camera.LegacyCamera;
+import net.smert.jreactphysics3d.framework.opengl.camera.LegacyCameraController;
+import net.smert.jreactphysics3d.framework.opengl.constants.GetString;
+import net.smert.jreactphysics3d.framework.opengl.constants.Light;
+import net.smert.jreactphysics3d.framework.opengl.mesh.Mesh;
+import net.smert.jreactphysics3d.framework.opengl.renderable.AbstractRenderable;
+import net.smert.jreactphysics3d.framework.utils.FpsTimer;
+import net.smert.jreactphysics3d.framework.utils.MemoryUsage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class MeshReaderTextured extends Screen {
+
+    private final static Logger log = LoggerFactory.getLogger(MeshReaderTextured.class);
+
+    private AbstractRenderable renderableCrateAndBarrel;
+    private FloatBuffer lightFloatBuffer;
+    private FpsTimer fpsTimer;
+    private LegacyCamera camera;
+    private LegacyCameraController cameraController;
+    private MemoryUsage memoryUsage;
+    private Mesh meshCrateAndBarrel;
+
+    public MeshReaderTextured(String[] args) {
+    }
+
+    private void handleInput() {
+        if (Fw.input.isKeyDown(Keyboard.ESCAPE) == true) {
+            Fw.app.stopRunning();
+        }
+        cameraController.update();
+    }
+
+    @Override
+    public void destroy() {
+        renderableCrateAndBarrel.destroy();
+        Fw.input.removeInputProcessor(cameraController);
+        Fw.input.releaseMouseCursor();
+    }
+
+    @Override
+    public void init() {
+
+        // Create timer
+        fpsTimer = new FpsTimer();
+
+        // Setup camera and controller
+        camera = new LegacyCamera();
+        camera.setPosition(0.0f, 0.0f, 5.0f);
+        cameraController = new LegacyCameraController(camera);
+
+        // Memory usage
+        memoryUsage = new MemoryUsage();
+
+        // Float buffer for light
+        lightFloatBuffer = GL.bufferHelper.createFloatBuffer(4);
+
+        // Create meshes
+        meshCrateAndBarrel = GL.mf.createMesh();
+
+        // Load the meshes from obj models
+        try {
+            Fw.graphics.loadMesh("crate_barrel/crate and barrel.obj", meshCrateAndBarrel);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        // Load textures
+        try {
+            Fw.graphics.loadTextures(meshCrateAndBarrel);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        // Create vertex buffer object renderables
+        renderableCrateAndBarrel = Fw.graphics.createVertexBufferObjectRenderable();
+        renderableCrateAndBarrel.create(meshCrateAndBarrel);
+
+        GL.o1.enableCulling();
+        GL.o1.cullBackFaces();
+        GL.o1.enableDepthTest();
+        GL.o1.setDepthFuncLess();
+        GL.o1.enableDepthMask();
+        GL.o1.setClearDepth(1.0f);
+        GL.o1.enableLight0();
+        GL.o1.enableLighting();
+        GL.o1.setSmoothLighting(true);
+        GL.o1.enableTexture2D();
+        GL.o1.clear();
+
+        GL.o1.setProjectionPerspective(
+                70.0f,
+                (float) Fw.config.getCurrentWidth() / (float) Fw.config.getCurrentHeight(),
+                0.05f, 512.0f);
+        GL.o1.setModelViewIdentity();
+
+        // Light position
+        lightFloatBuffer.put(0.0f);
+        lightFloatBuffer.put(15.0f);
+        lightFloatBuffer.put(10.0f);
+        lightFloatBuffer.put(1.0f);
+        lightFloatBuffer.flip();
+
+        log.info("OpenGL version: " + GL.o1.getString(GetString.VERSION));
+
+        Fw.input.addInputProcessor(cameraController);
+        Fw.input.grabMouseCursor();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void render() {
+        fpsTimer.update();
+        memoryUsage.update();
+
+        if (Fw.timer.isGameTick()) {
+            // Do nothing
+        }
+
+        if (Fw.timer.isRenderTick()) {
+            handleInput();
+
+            // Clear screen and reset modelview matrix
+            GL.o1.clear();
+            GL.o1.setModelViewIdentity();
+
+            camera.updateOpenGL();
+
+            GL.o1.light(Light.LIGHT0, Light.POSITION, lightFloatBuffer);
+
+            // Render directly
+            Fw.graphics.render(renderableCrateAndBarrel, -1.5f, -1.5f, 0.0f);
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        GL.o1.setViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void resume() {
+    }
+
+}
---
+++ examples/src/main/java/net/smert/jreactphysics3d/examples/vertexbufferobjectinterleaved/VertexBufferObjectInterleaved.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright 2014 Jason Sorensen (sorensenj@smert.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package net.smert.jreactphysics3d.examples.vertexbufferobjectinterleaved;
+
+import net.smert.jreactphysics3d.examples.common.CubeMeshForQuads;
+import net.smert.jreactphysics3d.examples.common.CubeMeshForQuadsWithPerVertexColors;
+import net.smert.jreactphysics3d.examples.common.CubeMeshForTriangles;
+import net.smert.jreactphysics3d.framework.Fw;
+import net.smert.jreactphysics3d.framework.Screen;
+import net.smert.jreactphysics3d.framework.helpers.Keyboard;
+import net.smert.jreactphysics3d.framework.opengl.GL;
+import net.smert.jreactphysics3d.framework.opengl.camera.LegacyCamera;
+import net.smert.jreactphysics3d.framework.opengl.camera.LegacyCameraController;
+import net.smert.jreactphysics3d.framework.opengl.constants.GetString;
+import net.smert.jreactphysics3d.framework.opengl.mesh.Mesh;
+import net.smert.jreactphysics3d.framework.opengl.renderable.AbstractRenderable;
+import net.smert.jreactphysics3d.framework.utils.FpsTimer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class VertexBufferObjectInterleaved extends Screen {
+
+    private final static Logger log = LoggerFactory.getLogger(VertexBufferObjectInterleaved.class);
+
+    private AbstractRenderable renderableQuads;
+    private AbstractRenderable renderableQuadsWithPerVertexColors;
+    private AbstractRenderable renderableTriangles;
+    private FpsTimer fpsTimer;
+    private LegacyCamera camera;
+    private LegacyCameraController cameraController;
+    private Mesh meshQuads;
+    private Mesh meshQuadsWithPerVertexColors;
+    private Mesh meshTriangles;
+
+    public VertexBufferObjectInterleaved(String[] args) {
+    }
+
+    private void handleInput() {
+        if (Fw.input.isKeyDown(Keyboard.ESCAPE) == true) {
+            Fw.app.stopRunning();
+        }
+        cameraController.update();
+    }
+
+    @Override
+    public void destroy() {
+        renderableQuads.destroy();
+        renderableQuadsWithPerVertexColors.destroy();
+        renderableTriangles.destroy();
+        Fw.input.removeInputProcessor(cameraController);
+        Fw.input.releaseMouseCursor();
+    }
+
+    @Override
+    public void init() {
+
+        // Create timer
+        fpsTimer = new FpsTimer();
+
+        // Setup camera and controller
+        camera = new LegacyCamera();
+        camera.setPosition(0.0f, 0.0f, 5.0f);
+        cameraController = new LegacyCameraController(camera);
+
+        // Create meshes
+        meshQuads = new CubeMeshForQuads();
+        meshQuadsWithPerVertexColors = new CubeMeshForQuadsWithPerVertexColors();
+        meshTriangles = new CubeMeshForTriangles();
+
+        // Create vertex buffer object interleaved renderables
+        renderableQuads = Fw.graphics.createVertexBufferObjectInterleavedRenderable();
+        renderableQuads.create(meshQuads);
+        renderableQuadsWithPerVertexColors = Fw.graphics.createVertexBufferObjectInterleavedRenderable();
+        renderableQuadsWithPerVertexColors.create(meshQuadsWithPerVertexColors);
+        renderableTriangles = Fw.graphics.createVertexBufferObjectInterleavedRenderable();
+        renderableTriangles.create(meshTriangles);
+
+        GL.o1.enableCulling();
+        GL.o1.cullBackFaces();
+        GL.o1.enableDepthTest();
+        GL.o1.setDepthFuncLess();
+        GL.o1.enableDepthMask();
+        GL.o1.setClearDepth(1.0f);
+        GL.o1.setSmoothLighting(true);
+        GL.o1.clear();
+
+        GL.o1.setProjectionPerspective(
+                70.0f,
+                (float) Fw.config.getCurrentWidth() / (float) Fw.config.getCurrentHeight(),
+                0.05f, 128.0f);
+        GL.o1.setModelViewIdentity();
+
+        log.info("OpenGL version: " + GL.o1.getString(GetString.VERSION));
+
+        Fw.input.addInputProcessor(cameraController);
+        Fw.input.grabMouseCursor();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void render() {
+        fpsTimer.update();
+
+        if (Fw.timer.isGameTick()) {
+            // Do nothing
+        }
+
+        if (Fw.timer.isRenderTick()) {
+            handleInput();
+
+            // Clear screen and reset modelview matrix
+            GL.o1.clear();
+            GL.o1.setModelViewIdentity();
+
+            camera.updateOpenGL();
+
+            // Render directly
+            Fw.graphics.render(renderableTriangles, -2.0f, 0.0f, 0.0f);
+            Fw.graphics.render(renderableQuads, 2.0f, 0.0f, 0.0f);
+            Fw.graphics.render(renderableQuadsWithPerVertexColors, 0.0f, 2.0f, 0.0f);
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        GL.o1.setViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void resume() {
+    }
+
+}
