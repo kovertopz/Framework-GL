629fad5c1342bbe7df62bfeb296f1a7bb9bf18ed
Jason Sorensen <git@smert.net>
2014-10-10 06:52:19 -0400

    Created a demo for dynamic vertex buffer objects.

---
+++ examples/src/main/java/net/smert/frameworkgl/examples/dynamicvertexbufferobject/DynamicSphereGameObject.java
@@ -0,0 +1,96 @@
+package net.smert.frameworkgl.examples.dynamicvertexbufferobject;
+
+import net.smert.frameworkgl.Fw;
+import net.smert.frameworkgl.gameobjects.GameObject;
+import net.smert.frameworkgl.math.MathHelper;
+import net.smert.frameworkgl.opengl.GL;
+import net.smert.frameworkgl.opengl.mesh.Mesh;
+import net.smert.frameworkgl.opengl.renderable.gl1.DynamicVertexBufferObjectRenderable;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class DynamicSphereGameObject extends GameObject {
+
+    private final static float SPHERE_UPDATE_DELTA = 1f / 60f;
+
+    private float sphereRadiansX;
+    private float sphereRadiansY;
+    private float sphereRadiansZ;
+    private float sphereRadiusX;
+    private float sphereRadiusY;
+    private float sphereRadiusZ;
+    private float sphereUpdateTimer;
+    private DynamicVertexBufferObjectRenderable dynamicVBORenderable;
+
+    @Override
+    public void init() {
+
+        // Set float values
+        sphereRadiansX = 0f;
+        sphereRadiansY = 1f;
+        sphereRadiansZ = 2f;
+        sphereRadiusX = 2f;
+        sphereRadiusY = 2f;
+        sphereRadiusZ = 2f;
+        sphereUpdateTimer = SPHERE_UPDATE_DELTA;
+
+        // Set position
+        getWorldTransform().setPosition(0f, 10f, 0f);
+
+        // Create initial mesh
+        Mesh dynamicMesh = GL.meshFactory.createMesh();
+        GL.dynamicMeshBuilder.
+                setColor(0, "medium_purple").
+                setQuality(8, 1, 1).
+                setRadius(sphereRadiusX, sphereRadiusY, sphereRadiusZ).
+                setSize(0f, 0f, 0f).
+                build("sphere").
+                createMesh(false, dynamicMesh);
+
+        // Create renderable
+        dynamicVBORenderable = Fw.graphics.createDynamicVertexBufferObjectRenderable();
+        dynamicVBORenderable.create(dynamicMesh);
+
+        // Attach to game object
+        setMesh(dynamicMesh);
+        setRenderable(dynamicVBORenderable);
+    }
+
+    @Override
+    public void update() {
+
+        // Update timer
+        float delta = Fw.timer.getDelta();
+        sphereUpdateTimer -= delta;
+
+        // Is it time to update?
+        if (sphereUpdateTimer < 0) {
+
+            // Reset timer
+            sphereUpdateTimer += SPHERE_UPDATE_DELTA;
+
+            // Update radians and radiuses
+            sphereRadiansX += SPHERE_UPDATE_DELTA * 3f;
+            sphereRadiansY -= SPHERE_UPDATE_DELTA * 3f;
+            sphereRadiansZ += SPHERE_UPDATE_DELTA * 3f;
+            float newSphereRadiusX = sphereRadiusX + ((MathHelper.Cos(sphereRadiansX) + 1) * 4f);
+            float newSphereRadiusY = sphereRadiusY + ((MathHelper.Cos(sphereRadiansY) + 1) * 4f);
+            float newSphereRadiusZ = sphereRadiusZ + ((MathHelper.Cos(sphereRadiansZ) + 1) * 2f);
+
+            // Generate new mesh
+            GL.dynamicMeshBuilder.
+                    setColor(0, "gold").
+                    setQuality(8, 1, 1).
+                    setRadius(newSphereRadiusX, newSphereRadiusY, newSphereRadiusZ).
+                    setSize(0f, 0f, 0f).
+                    build("sphere").
+                    createMesh(false, getMesh());
+
+            // Update renderable with new mesh
+            dynamicVBORenderable.update(getMesh());
+        }
+    }
+
+}
