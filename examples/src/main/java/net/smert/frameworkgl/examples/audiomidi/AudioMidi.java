2c0137f09f0cef77e946d14e3437f76f5bbf4fad
Jason Sorensen <git@smert.net>
2014-11-01 04:22:29 -0400

    Added a audio demo which plays a MIDI file. There is a short delay in the beginning of the audio file.

---
+++ examples/src/main/java/net/smert/frameworkgl/examples/audiomidi/AudioMidi.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2014 Jason Sorensen (sorensenj@smert.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package net.smert.frameworkgl.examples.audiomidi;
+
+import net.smert.frameworkgl.Fw;
+import net.smert.frameworkgl.Screen;
+import net.smert.frameworkgl.opengl.GL;
+import net.smert.frameworkgl.utils.FpsTimer;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class AudioMidi extends Screen {
+
+    private FpsTimer fpsTimer;
+
+    public AudioMidi(String[] args) {
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+        System.out.println(Fw.timer);
+    }
+
+    @Override
+    public void init() {
+        System.out.println("init");
+        fpsTimer = new FpsTimer();
+        Fw.audio.init();
+        Fw.audio.backgroundMusic("carminab.mid", true);
+        GL.o1.clear();
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("pause");
+        Fw.audio.pause("carminab.mid");
+    }
+
+    @Override
+    public void render() {
+        fpsTimer.update();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        System.out.println("resize");
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("resume");
+        Fw.audio.resume("carminab.mid");
+    }
+
+}
